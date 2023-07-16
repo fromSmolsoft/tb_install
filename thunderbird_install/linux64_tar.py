adbdab5b0dfa76320f945c2a1000110fee7b00c9
SmolSoft <smolsoft@email.cz>
2023-07-16 14:31:00 +0200

    checks for existing installation

--- thunderbird_install/linux64_tar.py
+++ thunderbird_install/linux64_tar.py
@@ -5,6 +5,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Check if Thunderbird has been installed using this script before
+if os.path.exists(f"{config['paths']['opt_path']}/thunderbird"):
+    # Move the current installation to a backup folder
+    os.system(f"mv {config['paths']['opt_path']}/thunderbird {os.path.expanduser('~')}/Thunderbird_backup")
+
 # Download Thunderbird
 os.system(f"wget -O {config['paths']['download_path']}/thunderbird.tar.bz2 '{config['links']['thunderbird_latest']}'")
 
@@ -15,7 +20,14 @@ os.system(f"tar xjf {config['paths']['download_path']}/thunderbird.tar.bz2 -C {c
 os.system(f"sudo mv {config['paths']['download_path']}/thunderbird {config['paths']['opt_path']}")
 
 # Create a symlink to the Thunderbird executable
-os.system(f"sudo ln -s {config['paths']['opt_path']}/thunderbird/thunderbird {config['paths']['bin_path']}/thunderbird")
+os.system(
+    f"sudo ln -sf {config['paths']['opt_path']}/thunderbird/thunderbird {config['paths']['bin_path']}/thunderbird")
+
+# Check if the desktop file has been installed using this script before
+if os.path.exists(f"{config['paths']['applications_path']}/thunderbird.desktop"):
+    # Move the current desktop file to a backup folder
+    os.system(
+        f"mv {config['paths']['applications_path']}/thunderbird.desktop {os.path.expanduser('~')}/Thunderbird_backup")
 
 # Download a copy of the desktop file
 os.system(f"sudo wget {config['links']['desktop_file']} -P {config['paths']['applications_path']}")
